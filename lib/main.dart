4815b477e9c3a3bbd91c1d1d65a2f617f3c51e49
Rajdeep Shah <rajdeep.shah@kisanwala.com>
2025-05-22 13:50:29 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class KisanFarmX extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'KisanFarmX Demo Home Page'),
     );
   }
 }
